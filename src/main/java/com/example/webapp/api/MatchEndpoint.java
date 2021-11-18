9d5db667f5d03060e731db86f9c98e1822a0ad7c
Maciej Rocker Wróblewski <maciej.rocker.wroblewski@gmail.com>
2021-11-18 13:20:35 +0100

    exception added

--- src/main/java/com/example/webapp/api/MatchEndpoint.java
+++ src/main/java/com/example/webapp/api/MatchEndpoint.java
@@ -2,7 +2,10 @@ package com.example.webapp.api;
 
 import com.example.webapp.service.MatchService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,25 +15,39 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MatchEndpoint {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MatchEndpoint.class);
+
     private final MatchService matchService;
 
     @GetMapping
-    public List<Match> getAll(){
+    public List<Match> getAll() {
         return matchService.getAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createMatch(@RequestBody Match match){
+    public void createMatch(@RequestBody Match match) {
         matchService.create(match);
     }
-@PutMapping
-    public void updateMatch(@RequestBody Match match){
+
+    @PutMapping
+    public void updateMatch(@RequestBody Match match) {
         matchService.update(match);
-}
-@DeleteMapping
+    }
+
+    @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
+
     public void deleteMatch(@RequestParam Long id) {
         matchService.delete(id);
-}
+    }
+
+    @ExceptionHandler(value = {IllegalStateException.class})
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
+        LOGGER.error("Error occured", ex);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
 }
--- src/main/java/com/example/webapp/repository/MatchRepository.java
+++ src/main/java/com/example/webapp/repository/MatchRepository.java
@@ -1,6 +1,5 @@
 package com.example.webapp.repository;
 
-import com.fasterxml.jackson.databind.deser.DataFormatReaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
