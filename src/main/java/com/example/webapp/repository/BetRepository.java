37a671e624b5011734d4e23ebffd51493d6ce9c9
Maciej Rocker Wróblewski <maciej.rocker.wroblewski@gmail.com>
2021-11-24 16:25:52 +0100

    spring data - query method

--- src/main/java/com/example/webapp/repository/BetRepository.java
+++ src/main/java/com/example/webapp/repository/BetRepository.java
@@ -2,7 +2,13 @@ package com.example.webapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BetRepository extends JpaRepository<BetEntity, Long> {
+List<BetEntity> findAllByUser_Id(Long userId);
+
+List<BetEntity> findAllByMatch_id(Long matchId);
+
 
 
 
--- src/main/java/com/example/webapp/service/BetService.java
+++ src/main/java/com/example/webapp/service/BetService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,35 +20,60 @@ public class BetService {
     private final MatchService matchService;
     private final BetRepository betRepository;
 
-    public void createBet(NewBet bet){
+    public void createBet(NewBet bet) {
         validateBet(bet);
 
-BetEntity entity = BetEntity.builder()
-        .firstTeamResult(bet.getFirstTeamResult())
-        .secondTeamResult(bet.getSecondTeamResult())
-        .match(MatchEntity.builder().id(bet.getMatchId()).build())
-        .user(UserEntity.builder().id(bet.getUserId()).build())
-        .build();
+        BetEntity entity = BetEntity.builder()
+                .firstTeamResult(bet.getFirstTeamResult())
+                .secondTeamResult(bet.getSecondTeamResult())
+                .match(MatchEntity.builder().id(bet.getMatchId()).build())
+                .user(UserEntity.builder().id(bet.getUserId()).build())
+                .build();
 
     }
 
-    public void deleteBet(Long id){
+    public void deleteBet(Long id) {
         betRepository.deleteById(id);
     }
-    
+
     public List<BetDetails> getAllForUser(Long userId) {
-        return null;
+        return betRepository.findAllByUser_Id(userId)
+                .stream()
+                .map(ent -> BetDetails.builder()
+                        .id(ent.getId())
+                        .firstTeam(ent.getMatch().getFirstTeam())
+                        .secondTeam(ent.getMatch().getSecondTeam())
+                        .startTime(ent.getMatch().getStartTime())
+                        .userLogin(ent.getUser().getLogin())
+                        .userName(ent.getUser().getFirstName() + " " + ent.getUser().getLastName())
+                        .firstTeamResult(ent.getFirstTeamResult())
+                        .secondTeamResult(ent.getSecondTeamResult())
+                        .build())
+                .collect(Collectors.toList());
     }
 
-    public List<BetDetails> getAllForMatch(Long matchId){
-        return null;
+
+    public List<BetDetails> getAllForMatch(Long matchId) {
+        return betRepository.findAllByMatch_id(matchId)
+                .stream()
+                .map(ent -> BetDetails.builder()
+                        .id(ent.getId())
+                        .firstTeam(ent.getMatch().getFirstTeam())
+                        .secondTeam(ent.getMatch().getSecondTeam())
+                        .startTime(ent.getMatch().getStartTime())
+                        .userLogin(ent.getUser().getLogin())
+                        .userName(ent.getUser().getFirstName() + " " + ent.getUser().getLastName())
+                        .firstTeamResult(ent.getFirstTeamResult())
+                        .secondTeamResult(ent.getSecondTeamResult())
+                        .build())
+                .collect(Collectors.toList());
     }
 
     private void validateBet(NewBet bet) {
-        if (!matchService.checkIfMatchExists(bet.getMatchId())){
+        if (!matchService.checkIfMatchExists(bet.getMatchId())) {
             throw new MatchNotFoundException("Mecz nie istnieje.");
         }
-        if (bet.getFirstTeamResult() < 0 || bet.getSecondTeamResult() < 0){
+        if (bet.getFirstTeamResult() < 0 || bet.getSecondTeamResult() < 0) {
             throw new IllegalArgumentException("Wynik nie może być ujemny");
         }
     }
