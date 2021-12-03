18784b5654197b30a5effba0518b308971bda3e5
Maciej Rocker Wróblewski <maciej.rocker.wroblewski@gmail.com>
2021-12-03 23:31:48 +0100

    mvc - form addUser

---
+++ src/main/java/com/example/webapp/web/UserController.java
@@ -0,0 +1,33 @@
+package com.example.webapp.web;
+
+import com.example.webapp.api.model.User;
+import com.example.webapp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ModelAndView displayAddUserPage(){
+        ModelAndView mav = new ModelAndView("addUser");
+        mav.addObject("user", new User());
+        return mav;
+    }
+
+    @PostMapping
+    public RedirectView handleAddUser(@ModelAttribute("user") User user){
+        userService.create(user);
+        return new RedirectView("/");
+    }
+}
