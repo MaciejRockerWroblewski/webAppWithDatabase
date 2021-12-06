bda6d5cc7f0d8cb7aa5cc68429db7fb5107647bd
Maciej Rocker Wróblewski <maciej.rocker.wroblewski@gmail.com>
2021-12-06 15:03:40 +0100

    mvc - form - select + each

---
+++ src/main/java/com/example/webapp/web/model/SelectOption.java
@@ -0,0 +1,13 @@
+package com.example.webapp.web.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class SelectOption {
+    private Long id;
+    private String label;
+}
