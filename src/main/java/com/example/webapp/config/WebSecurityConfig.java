e20eefa8d0c0878bc030d8337e439f256abe5cdd
Maciej Rocker Wróblewski <maciej.rocker.wroblewski@gmail.com>
2021-12-12 19:42:46 +0100

    spring security - in memory authentication

--- src/main/java/com/example/webapp/config/WebSecurityConfig.java
+++ src/main/java/com/example/webapp/config/WebSecurityConfig.java
@@ -1,24 +1,39 @@
 package com.example.webapp.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("test").roles("ADMIN")
+                .and().withUser("user").password("user").roles("USER");
+    }
+
         @Override
     protected void configure (HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/user/**").authenticated()
                 .and().authorizeRequests().antMatchers("/match/all").permitAll()
-                .and().authorizeRequests().antMatchers("/match", "/match/details*", "/match/edit*").authenticated()
-                .and().authorizeRequests().antMatchers("/bet/**").authenticated()
+                .and().authorizeRequests().antMatchers("/api/**", "/match", "/match/details*", "/match/edit*", "/user/**", "/bet/**")
+                .hasRole("USER")
+                .and().authorizeRequests().antMatchers("/match", "/match/details*", "/match/edit*", "/match/delete", "/user/**")
+                .hasRole("ADMIN")
                 .and().formLogin()
                 .and().logout()
-                .and().csrf().disable()
-                .addFilter(new BasicAuthenticationFilter());
-    }
+                .and().csrf().disable();
+                    }
 }
